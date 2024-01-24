082b4161d8169ea4bffee7d5ad074cbf6553e3c9
Almir Nakicevic <a.nakicevic@overlap.at>
2024-01-24 16:29:02 +0100

    some changes

--- static/chunks/576.3d23c234d526dbc9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[576],{4576:function(e,t,n){n.r(t);var r=n(5893);t.default=()=>(0,r.jsx)("svg",{width:"6",height:"11",viewBox:"0 0 6 11",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:(0,r.jsx)("path",{d:"M5 9.5L1 5.5L5 1.5",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"})})}}]);
